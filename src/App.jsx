8d8b4694837d209671f01c4bcd3ac6f6769d7252
JoeA <joealexanderadams@gmail.com>
2024-02-05 10:49:40 +0000

    add articles list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import axios from 'axios'
 import './App.css'
+import ViewArticles from './Components/ViewArticles'
+
+export const newsApi = axios.create({
+  baseURL: "https://nc-news-wpw8.onrender.com/api/"
+})
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      
+      <ViewArticles/>
     </>
   )
 }
