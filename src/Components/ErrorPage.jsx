5151d871c9ba1a5eb5f99fa22ed9ef88f0ba9ff1
JoeA <joealexanderadams@gmail.com>
2025-04-07 19:44:30 +0100

    add more information to error page

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
-import "../CSS/errorPage.css"
+import "../CSS/errorPage.css";
 
 export default function ErrorPage() {
     return (
         <section className="error-page">
             <h1>404 Page not found</h1>
-            <Link to="/" id="return">Return to homepage</Link>
+            <h2>
+                Sometimes the data needs time to load, please wait 1-2 mintutes before refreshing
+            </h2>
+            <Link to="/" id="return">
+                Return to homepage
+            </Link>
         </section>
     );
 }
